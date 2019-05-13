22a20a2ad1c6dabf5c27bd322c8109131c8c7833
mx623303468 <623303468@qq.com>
2019-05-13 17:05:56 +0800

    管理输出-设置HtmlWebpackPlugin,使用CleanWebpackPlugin清理/dist文件夹

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -9,4 +11,11 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出',
+      appMountId: 'app'
+    }),
+    new CleanWebpackPlugin()
+  ]
 };
