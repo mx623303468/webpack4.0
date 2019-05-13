a1089546706e6c95860a38b98cc25b139636eac9
mx623303468 <623303468@qq.com>
2019-05-13 16:29:03 +0800

    管理资源-加载 font 字体

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
       {
         test: /\.(png|jpg|jgeg|gif|svg)$/,
         use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
       }
     ]
   }
