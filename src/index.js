0b5104dd0a1b75a108825d703985661edbafeb26
mx623303468 <623303468@qq.com>
2019-05-13 16:22:48 +0800

    管理资源-加载images图像

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash'
 import './style.css'
+import avatar from './avatar.jpg'
 
 function component() {
     var ele = document.createElement('div')
     ele.innerHTML = _.join(['Hello', 'webpack'], ' ')
     ele.classList.add('hello')
+
+    var icon = new Image();
+    icon.src = avatar;
+
+    ele.appendChild(icon)
 
     return ele;
 }
