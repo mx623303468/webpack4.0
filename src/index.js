949362b3977101e0887966ad809d0fd4177fbec6
mx623303468 <623303468@qq.com>
2019-05-13 16:54:30 +0800

    管理输出-预先准备

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import _ from 'lodash'
+import printMe from './print';
 
 function component() {
     var ele = document.createElement('div')
+    var btn = document.createElement('button')
+
     ele.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+    btn.innerHTML = '点击这里，然后查看 console'
+    btn.onclick = printMe
+
+    ele.appendChild(btn)
+
 
     return ele;
 }
