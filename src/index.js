5b711976dc65dd1ee88af691a1bd9ad586641d81
mx623303468 <623303468@qq.com>
2019-05-13 16:45:02 +0800

    管理资源-回退处理 为下一章管理输出做准备

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import _ from 'lodash'
-import './style.css'
-import avatar from './avatar.jpg'
-import Data from './data.xml'
 
 function component() {
     var ele = document.createElement('div')
     ele.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    ele.classList.add('hello')
-
-    var icon = new Image();
-    icon.src = avatar;
-
-    ele.appendChild(icon)
-
-    console.log(Data);
 
     return ele;
 }
